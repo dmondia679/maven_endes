7ba1c834c1e219fb4c43d09d281bca39c80ed08b
David Montes Diaz <dmondia679@g.educaand.es>
2024-03-19 19:42:00 +0100

    nuevos cambios

---
+++ src/main/java/com/miempresa/app/Employee.java
@@ -0,0 +1,12 @@
+package com.miempresa.app;
+
+public class Employee {
+	private int años;
+	private String nombre;
+	
+	public Employee(String nombre, int años) {
+		this.años=años;
+		this.nombre=nombre;
+	}
+	
+}
